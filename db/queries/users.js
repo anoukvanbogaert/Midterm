30e365ae81dacc62ade77eb3b3a87ad49ff823f4
Alexander Sidorenko <asiddev@protonmail.com>
2022-10-22 13:03:37 -0700

    update pull

--- db/queries/users.js
+++ db/queries/users.js
@@ -1,12 +1,10 @@
-const {Pool} = require('pg/lib');
-const db = require('../connection');
+const { Pool } = require("pg/lib");
+const db = require("../connection");
 
 const getUsers = () => {
-  return db.query('SELECT * FROM users;')
-    .then(data => {
-      return data.rows;
-    });
+  return db.query("SELECT * FROM users;").then((data) => {
+    return data.rows;
+  });
 };
 
-
-module.exports = {getUsers};
+module.exports = { getUsers };
