de4098b3bda1c348373600a16b9bc34d936c7058
EVillett <emily.villett@gmail.com>
2022-10-22 10:47:24 -0700

    upading database

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,4 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (id, name, email) VALUES (1, 'Alice', 'alice@email.com');
+INSERT INTO users (id, name, email) VALUES (2, 'Kira', 'kira@email.com');
+INSERT INTO users (id, name, email) VALUES (3, 'Ryan', 'ryan@emai.com');
