20862716bf60e3656b30bf84fa2aa0c2742d5b80
Alexander Sidorenko <asiddev@protonmail.com>
2022-10-25 14:44:23 -0700

    add takeQuiz page with jquery script. Create css and also logic behind linking scores

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,47 @@
-// Client facing scripts here
+/* eslint-disable no-undef */
+
+$("document").ready(function () {
+  let total = 0;
+  let attempts = 0;
+  let numberOfQuestions = $("div[class='card-header']").length;
+  let correctAwnser = "DOG";
+
+  $(".testbutton").click(function () {
+    $(this).toggleClass("hightlight");
+    $(this).siblings().removeClass("hightlight");
+
+    // console.log(total);
+
+    if ($(this).val() === correctAwnser) {
+      if (total >= 0 && total <= numberOfQuestions) {
+        total++;
+      } else {
+        total--;
+      }
+    }
+  });
+  $("#quizSubmit").submit(function (event) {
+    $(".testbutton").prop("disabled", true);
+
+    event.preventDefault();
+    attempts++;
+
+    if (total === numberOfQuestions) {
+      $(".testbutton").css("color", "black");
+      $("#score").append(`<div>
+      <p>Attempts : ${attempts}</p>
+      <h1 class="green">YOUR ON FIRE!🔥 </h1>
+      <h2>${total}/${numberOfQuestions} </h2>
+      <button class='backhHome'><a href="/quizzes/myquizzes">Back to My Quizzes</a></button>
+      </div>`);
+      total = 0;
+      alert("Congrats! You got them all right!");
+    } else if (total < numberOfQuestions) {
+      console.log($(".testbutton").hasClass("hightlight").length);
+
+      $("#score").append(`<div>
+      <button class='backhHome'><a href="/quizzes/myquizzes">Back to My Quizzes</a></button>
+      </div>`);
+    }
+  });
+});
