3eae2c6bd2d5c3bf4c706238127ba62f4728f122
Alexander Sidorenko <asiddev@protonmail.com>
2022-10-25 09:29:44 -0700

    removed workspace file

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,14 +1,14 @@
 -- USERS
-INSERT INTO users (name, email) 
-VALUES 
+INSERT INTO users (name, email)
+VALUES
 ('Paris', 'paris@email.com'),
-('Lindsey', 'lindsey@lhl.ca'), 
-('Bondy', 'bondy@lhl.ca'); 
+('Lindsey', 'lindsey@lhl.ca'),
+('Bondy', 'bondy@lhl.ca');
 
 
--- QUIZZES 
-INSERT INTO quizzes (name, user_id, likes, rating) 
-VALUES 
+-- QUIZZES
+INSERT INTO quizzes (name, user_id, likes, rating)
+VALUES
 ('Easy general knowledge', 1, 18, 4.5 ),
 ('Geography Trivia', 2, 35, 4.2),
 ('Music Trivia', 3, 12, 3.5);
@@ -16,7 +16,7 @@ VALUES
 
 -- QUESTIONS
 INSERT INTO questions (quiz_id, question,, option_1, option_2, option_3)
-VALUES 
+VALUES
 -- QUIZ QUESTIONS FOR QUIZ 1
 (1, 'Which one of the following scientists is known for their contributions to the science of evolution?', 'Marie Curie', 'Thomas Edison', 'Stephen Hawking', 'Charles Darwin'),
 (1, 'Which fruit is associated with Isaac Newton?', 'Apple', 'Pear', 'Pineapple', 'Banana'),
