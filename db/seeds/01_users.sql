e84fa69421ae02314de14d88820daf9524881e09
EVillett <emily.villett@gmail.com>
2022-10-27 21:48:08 -0700

    adding more seeds to DB

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -3,7 +3,8 @@ INSERT INTO users (name, email)
 VALUES
 ('Paris', 'paris@email.com'),
 ('Lindsey', 'lindsey@lhl.ca'),
-('Bondy', 'bondy@lhl.ca');
+('Bondy', 'bondy@lhl.ca'),
+('Steven', 'steven@lhl.ca');
 
 
 -- QUIZZES
@@ -11,7 +12,9 @@ INSERT INTO quizzes (name, user_id, likes, rating, listed)
 VALUES
 ('Easy general knowledge', 1, 18, 4.5, TRUE),
 ('Geography Trivia', 2, 35, 4.2, TRUE),
-('Music Trivia', 3, 12, 3.5, TRUE);
+('Music Trivia', 3, 12, 3.5, TRUE),
+('Do you know cats?', 4, 23, 5, TRUE),
+('Do you wear wigs?', 1, 21, 5, TRUE);
 
 
 -- QUESTIONS
@@ -28,11 +31,17 @@ VALUES
 -- QUIZ QUESTIONS FOR QUIZ 3
 (3, 'How many lines are on the Spotify logo?', '3', '4', '2', '5'),
 (3, 'What was Madonnas first top 10 hit?', 'Holiday', 'Like a Prayer', 'La Isla Bonita', 'Hung Up'),
-(3, 'Which classical composer was deaf?', 'Beethoven', 'Mozart', 'Bach', 'Chopin');
+(3, 'Which classical composer was deaf?', 'Beethoven', 'Mozart', 'Bach', 'Chopin'),
+-- QUIZ QUESTIONS FOR QUIZ 4
+(4, 'What percent of a house cats genome is tiger?', '95.6%', '76.5%', '50%', '65%'),
+(4, 'How high can a cat jump?', 'Up to six times their height', 'Six feet', 'My cat is lazy', 'Nine feet'),
+(4, 'How many bones do cats have?', '230', '150', '400', '300'),
+--QUIZ QUESTIONS FOR QUIZ 5
+(5, 'Well, do you wear wigs?', 'Uh, no I do not', 'Yes', 'No', 'Maybe'),
+(5, 'Have you worn wigs?', 'No I have not', 'Yes', 'No', 'Maybe'),
+(5, 'Will you wear wigs?', 'Maybe?', 'Yes', 'No', 'Maybe'),
+(5, 'When will you wear wigs?', '*LAUGHS*', 'Yes', 'No', 'Maybe');
 
--- RESULTS
-
--- ANSWERS tbd
 
 
 
